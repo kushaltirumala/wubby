011b61f61809d0cf6dfed8a2caf100ed7a89178a
kNelakonda <kavi.nelakonda@gmail.com>
2016-05-06 16:16:16 -0700

    small fix to make it work with filters

--- webcammy/src/webcammy/VideoPanel.java
+++ webcammy/src/webcammy/VideoPanel.java
@@ -13,70 +13,68 @@ import javax.swing.border.Border;
 import org.opencv.core.Core;
 
 public class VideoPanel extends JPanel implements ActionListener {
-	
-    public VideoCap videoCap = new VideoCap();
-	 JButton blackFilter = new JButton("change filter to black");
 
-    public VideoPanel() {
-    	super();
-    }
- 
-    public void paint(Graphics g){
-        paintComponent(g);
-        g.drawImage(videoCap.getOneFrame(), 0,0, this);
-    }
-    
-    public static void main(String[] args) {
-    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        JFrame frame = new JFrame();
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setBounds(100, 100, 1000, 1000);
-        frame.setLayout(new BorderLayout());
-        
-        VideoPanel vid = new VideoPanel();
-        JPanel buttons = new JPanel();
-        buttons.setLayout(new FlowLayout());
-        
-        frame.setVisible(true);
+	public VideoCap videoCap = new VideoCap();
+	JButton blackFilter = new JButton("change filter to black");
 
-        frame.add(vid, BorderLayout.CENTER);
-        frame.add(buttons, BorderLayout.SOUTH);
-        
-        JButton blackFilter = new JButton("change filter to black");
-    	buttons.add(blackFilter, BorderLayout.SOUTH);
-    	blackFilter.addActionListener(vid);
-    	
-    	JButton takePic = new JButton("take a picture");
-    	buttons.add(takePic, BorderLayout.SOUTH);
-    	takePic.addActionListener(vid);
-    	
-    	JButton trippyFilter = new JButton("change filter to trippy");
-    	buttons.add(trippyFilter, BorderLayout.SOUTH);
-    	trippyFilter.addActionListener(vid);
-        
-    	
-    	
-    	vid.setVisible(true);
-        
-        while(true){
-        	vid.repaint();
-        }
-    }
+	public VideoPanel() {
+		super();
+	}
+
+	public void paint(Graphics g) {
+		paintComponent(g);
+		g.drawImage(videoCap.getOneFrame(), 0, 0, this);
+	}
+
+	public static void main(String[] args) {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		JFrame frame = new JFrame();
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(100, 100, 1000, 1000);
+		frame.setLayout(new BorderLayout());
+
+		VideoPanel vid = new VideoPanel();
+		JPanel buttons = new JPanel();
+		buttons.setLayout(new FlowLayout());
+
+		frame.setVisible(true);
+
+		frame.add(vid, BorderLayout.CENTER);
+		frame.add(buttons, BorderLayout.SOUTH);
+
+		JButton blackFilter = new JButton("change filter to black");
+		buttons.add(blackFilter, BorderLayout.SOUTH);
+		blackFilter.addActionListener(vid);
+
+		JButton takePic = new JButton("take a picture");
+		buttons.add(takePic, BorderLayout.SOUTH);
+		takePic.addActionListener(vid);
+
+		JButton trippyFilter = new JButton("change filter to trippy");
+		buttons.add(trippyFilter, BorderLayout.SOUTH);
+		trippyFilter.addActionListener(vid);
+
+		vid.setVisible(true);
+
+		while (true) {
+			vid.repaint();
+		}
+	}
 
 	public void actionPerformed(ActionEvent e) {
-		JButton source = (JButton)e.getSource();
-		if(source.getText().equals("change filter to black")) {
+		JButton source = (JButton) e.getSource();
+		if (source.getText().equals("change filter to black")) {
 			System.out.println("black");
 			videoCap.changeFilter(new BlackWhiteFilter());
 		} else if (source.getText().equals("change filter to trippy")) {
 			System.out.println("edge detection");
 			videoCap.changeFilter(new EdgeFilter());
-		} else if (source.getText().equals("take a picture")){
+		} else if (source.getText().equals("take a picture")) {
 			System.out.println("picture");
 			File outputfile = new File("image.jpg");
 			try {
-				ImageIO.write(videoCap.getImage(videoCap.getMat()), "jpg", outputfile);
+				ImageIO.write(videoCap.getOneFrame(), "jpg", outputfile);
 			} catch (IOException e1) {
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
@@ -84,4 +82,3 @@ public class VideoPanel extends JPanel implements ActionListener {
 		}
 	}
 }
-
