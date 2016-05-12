e77f3d096b18317488cffb7d2d19b3abb5688a72
Kushal Tirumala <kushaltirumala99@gmail.com>
2016-05-11 18:31:22 -0700

    mm

---
+++ webcammy/src/webcammy/ImageFilter.java
@@ -0,0 +1,14 @@
+package webcammy;
+
+import java.awt.image.BufferedImage;
+
+public class ImageFilter implements ScreenFilter {
+
+	public ImageFilter() {}
+	
+	
+
+	public BufferedImage filter(BufferedImage img) {
+		return img;
+	}
+}
