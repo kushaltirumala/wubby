12a7675f4bdc7c659c2d3b64d7c4ed704d582085
kNelakonda <kavi.nelakonda@gmail.com>
2016-05-16 14:56:04 -0700

    hahahahhahhahahahahhahhahahahhhahhafuckmeamiritelololololololololololkapakapkapakpakpakapkapakpakpakaT R I G G E R E D

---
+++ webcammy/src/webcammy/JUWebcammyTest.java
@@ -0,0 +1,115 @@
+package webcammy;
+
+import org.junit.Test;
+
+public class JUWebcammyTest {
+	
+	@Test
+	public void imageFilterConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void imageFilterFilter()
+	{
+		
+	}
+	
+	@Test
+	public void blackWhiteFilterConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void blackWhiteFilterTurnRGBToGrayscale()
+	{
+		
+	}
+	
+	@Test
+	public void blackWhiteFilterFilter()
+	{
+		
+	}
+	
+	@Test
+	public void edgeFilterConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void edgeFilterColorDistance()
+	{
+		
+	}
+	
+	@Test
+	public void edgeFilterFilter()
+	{
+		
+	}
+	
+	@Test
+	public void negativeFilterConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void negativeFilterTurnRGBToNegative()
+	{
+		
+	}
+	
+	@Test
+	public void negativeFilterFilter()
+	{
+		
+	}
+	
+	@Test
+	public void topPanelConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void topPanelActionPerformed()
+	{
+		
+	}
+	
+	@Test
+	public void videoButtonsConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void videoButtonsActionPerformed()
+	{
+		
+	}
+	
+	@Test
+	public void videoCapConstructor()
+	{
+		
+	}
+	
+	@Test
+	public void videoCapChangeFilter()
+	{
+		
+	}
+	
+	@Test
+	public void videoCapGetFPS()
+	{
+		
+	}
+	
+}
